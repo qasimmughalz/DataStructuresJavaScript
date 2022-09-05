4a5a17b13eb0936cfa10570ab8453563edd93017
Qasim Mughal <imrqasim@gmail.com>
2022-09-05 15:46:22 +0500

    Setting Things Up

---
+++ LinkedList/server.js
@@ -0,0 +1,12 @@
+
+const http = require('http')
+const PORT = process.env.PORT || 8000
+const createServer = http.createServer()
+
+
+
+
+
+
+
+createServer.listen( PORT , ()=> `Listening to the Port ${PORT}`)
